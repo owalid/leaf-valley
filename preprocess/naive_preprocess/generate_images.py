7185225809bde7caf9e3091acce6a8600e1d994b
owalid <othmane.el.ayadi@gmail.com>
2022-03-07 11:30:16 +0100

    add naive preprocess imgs

--- preprocess/naive_preprocess/generate_images.py
+++ preprocess/naive_preprocess/generate_images.py
@@ -18,12 +18,11 @@ def safe_open_w(path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     return open(path, 'w')
 
-def delete_bg(specie, img_number, gray_scale):
-  path_img = f"../../data/augmentation/{specie.capitalize()}___healthy/image ({img_number}).JPG"  
+def delete_bg(specie_directory, img_number, type_img):
+  path_img = f"../../data/augmentation/{specie_directory}/image ({img_number}).JPG"  
 
-
-  rgb_img, path, filename = pcv.readimage(path_img, mode='rgb')
-  gray_img, path_grat, filename_gray = pcv.readimage(path_img, mode='gray')
+  rgb_img, _, _ = pcv.readimage(path_img, mode='rgb')
+  gray_img, _, _ = pcv.readimage(path_img, mode='gray')
 
 
   lab = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
@@ -32,74 +31,79 @@ def delete_bg(specie, img_number, gray_scale):
   lab[:,:,2] = lab[:,:,2]/10 # B
   new_rgb_img = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
   
+  # Create mask from a channel
   a_mask = pcv.rgb2gray_lab(rgb_img=new_rgb_img, channel='a')
   a_mask = np.where(a_mask <= int(a_mask.mean()), 0, a_mask)
   a_mask = np.where(a_mask > 0, 1, a_mask)
+
+  # Get countours and fill inside contours
   cnts = cv2.findContours(a_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   cnts = cnts[0] if len(cnts) == 2 else cnts[1]
   cv2.fillPoly(a_mask, cnts, (255,255,255))
-  result = cv2.bitwise_and(rgb_img,rgb_img,mask=a_mask)
+  cv2.bitwise_and(rgb_img,rgb_img,mask=a_mask)
 
-  masked_image = pcv.apply_mask(img=gray_img, mask=a_mask, mask_color='black')
 
+  img = rgb_img if type_img == 'color' else gray_img
+
+  # Apply mask and add sharpness
+  masked_image = pcv.apply_mask(img=img, mask=a_mask, mask_color='black')  
   im = Image.fromarray(masked_image)
   enhancer = ImageEnhance.Sharpness(im)
-  factor = 2
-  im_s_1 = enhancer.enhance(factor)
+  im_s_1 = enhancer.enhance(2)
 
-  if gray_scale:
-    return im_s_1
+  if type_img == 'canny':
+    edges = pcv.canny_edge_detect(np.array(im_s_1))
+    return Image.fromarray(edges)
 
-
-  edges = pcv.canny_edge_detect(np.array(im_s_1))
-  return Image.fromarray(edges)
-
-
-
+  return im_s_1
 
 if __name__ == '__main__':
-  res_augmented = input('use not augmented or augmented ?\n0 for not augmented\n1 for augmented\n> ')
-  res_augmented = int(res_augmented)
+  res_augmented = input('use not augmented or augmented ?\n0 for not augmented (default)\n1 for augmented\n> ')
 
-  if res_augmented not in [0, 1]:
-    print('you need to select 0 or 1')
-    exit(1)
-  if res_augmented == 0:
+  if res_augmented == '1':
     res_augmented = 'no_augmented'
     df = getDf('../../data/no_augmentation')
   else:
     res_augmented = 'augmented'
     df = getDf('../../data/augmentation')
 
-  gray_scale = input(f'Choose your result image: \n0 for canny_edge\n1 for gray_scale\n> ')
-  gray_scale = bool(int(gray_scale))
-  
-  if gray_scale:
-    gray_scale_prefix = 'gray'
+  type_img_code = input(f'Choose your result type image: \n0 for canny_edge\n1 for gray_scale (default)\n2 for rgb\n> ')
+  if type_img_code == '0':
+    type_img = 'canny'
+  elif type_img_code == '2':
+    type_img = 'color'
   else:
-    gray_scale_prefix = 'canny'
+    type_img = 'gray'
   
-  default_path_result = f"../../data/preprocess/{res_augmented}/{gray_scale_prefix}"
-  choose_final_path = input(f'choose your final path \n(default is: {default_path_result}/<specie>/<number>.jpg)\n> ')
+  default_path_result = f"../../data/preprocess/{res_augmented}/{type_img}"
+  choose_final_path = input(f'Choose your final path \n(default is: {default_path_result}/<specie>/<number>.jpg)\n> ')
 
   if choose_final_path == '':
     choose_final_path = default_path_result
 
-
-  df_filtred = df.loc[(df['specie'] != 'corn') & (df['healthy'])]
+  specie_dont_want = ['corn', 'background_without_leaves']
+  df_filtred = df.loc[(~df['specie'].isin(specie_dont_want)) & (df['healthy'])]
+  df_others_specie = df.loc[(~df['specie'].isin(specie_dont_want)) & (~df['specie'].isin(list(df_filtred.specie.values)))]
+  df_filtred = pd.concat([df_others_specie, df_filtred])
   indexes_species = df_filtred.index
 
-  for index_specie in indexes_species:
-    current_df = df_filtred.loc[index_specie]
-    print(f"[+] specie: {current_df.specie}")
-    print(f"[+] number of images: {current_df.number_img}")
+  print('\n')
+  print("===========================================")
+  print(f"[+] type dataset: {res_augmented}")
+  print(f"[+] type image: {type_img}")
+  print(f"[+] path: {choose_final_path}")
+  for specie_directory in indexes_species:
+    current_df = df_filtred.loc[specie_directory]
+    print(f"[+] index {specie_directory}")
+    print(f"[+] Start generate specie: {current_df.specie}")
+    print(f"[+] Number of images: {current_df.number_img}")
     specie = current_df.specie
+    
     for index in range(1, current_df.number_img):
       if int(current_df.number_img / 2) == index:
         print("[+] 50%")
-      new_img = delete_bg(specie, index, gray_scale)
+      new_img = delete_bg(specie_directory, index, type_img)
       file_path = f"{choose_final_path}/{specie}/{index}.jpg"
       with safe_open_w(file_path) as f:
         new_img.save(f)
-    print(f"---- end with {specie} ----")
-  
+    print(f"[+] End with {specie}")
