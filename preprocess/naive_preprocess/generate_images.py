69e8e52064edca23a770dfcb88888501f33a6ec8
owalid <othmane.el.ayadi@gmail.com>
2022-03-08 17:29:21 +0100

    continue naive generate_img

--- preprocess/naive_preprocess/generate_images.py
+++ preprocess/naive_preprocess/generate_images.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import cv2
+import joblib
 import numpy as np
 from plantcv import plantcv as pcv
 from PIL import Image, ImageEnhance
@@ -8,12 +9,14 @@ import pandas as pd
 import sys
 sys.path.append("/Users/agritech/vic-2_i/analysis/directory_data_repartitions")
 from utils import getDf
+import joblib
 
 pcv.params.debug = ''
 debug = ''
 
 def safe_open_w(path):
-    ''' Open "path" for writing, creating any parent directories as needed.
+    '''
+      Open "path" for writing, creating any parent directories as needed.
     '''
     os.makedirs(os.path.dirname(path), exist_ok=True)
     return open(path, 'w')
@@ -53,19 +56,40 @@ def delete_bg(specie_directory, img_number, type_img):
 
   if type_img == 'canny':
     edges = pcv.canny_edge_detect(np.array(im_s_1))
-    return Image.fromarray(edges)
+    normalized_img = cv2.normalize(edges, edges, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    return Image.fromarray(edges), cv2.resize(normalized_img, (204, 204))
+  else:
+    array_img = np.array(im_s_1)
+    if type_img == 'color':
+      normalized_img = cv2.normalize(array_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    else:
+      normalized_img = cv2.normalize(array_img, array_img, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-  return im_s_1
+  return im_s_1, cv2.resize(normalized_img, (204, 204))
 
+
+
+
+
+
+
+
+
+
+# MAIN
 if __name__ == '__main__':
   res_augmented = input('use not augmented or augmented ?\n0 for not augmented (default)\n1 for augmented\n> ')
 
   if res_augmented == '1':
-    res_augmented = 'no_augmented'
-    df = getDf('../../data/no_augmentation')
-  else:
     res_augmented = 'augmented'
     df = getDf('../../data/augmentation')
+  else:
+    res_augmented = 'no_augmented'
+    df = getDf('../../data/no_augmentation')
+
+  data_used = int(input('Choose number of images per class maximum 1000 (default maximum per specie)\n> '))
+  data_used =  data_used if data_used <= 1000 else -1
+
 
   type_img_code = input(f'Choose your result type image: \n0 for canny_edge\n1 for gray_scale (default)\n2 for rgb\n> ')
   if type_img_code == '0':
@@ -78,6 +102,10 @@ if __name__ == '__main__':
   default_path_result = f"../../data/preprocess/{res_augmented}/{type_img}"
   choose_final_path = input(f'Choose your final path \n(default is: {default_path_result}/<specie>/<number>.jpg)\n> ')
 
+  generate_pickle = input(f'Want to generate pickle (default Y) ? Y/n\n> ')
+
+  generate_pickle = 'y' if generate_pickle == '' else generate_pickle
+
   if choose_final_path == '':
     choose_final_path = default_path_result
 
@@ -87,23 +115,42 @@ if __name__ == '__main__':
   df_filtred = pd.concat([df_others_specie, df_filtred])
   indexes_species = df_filtred.index
 
+  data = dict()
+  data['label'] = []
+  data['img_data'] = []
+
   print('\n')
-  print("===========================================")
+  print("=====================================================")
   print(f"[+] type dataset: {res_augmented}")
   print(f"[+] type image: {type_img}")
   print(f"[+] path: {choose_final_path}")
+  print("=====================================================")
   for specie_directory in indexes_species:
     current_df = df_filtred.loc[specie_directory]
+    number_img = current_df.number_img if data_used == -1 else data_used
     print(f"[+] index {specie_directory}")
     print(f"[+] Start generate specie: {current_df.specie}")
-    print(f"[+] Number of images: {current_df.number_img}")
+    print(f"[+] Number of images: {number_img}")
     specie = current_df.specie
-    
-    for index in range(1, current_df.number_img):
-      if int(current_df.number_img / 2) == index:
+
+    for index in range(1, number_img):
+      if int(number_img / 2) == index:
         print("[+] 50%")
-      new_img = delete_bg(specie_directory, index, type_img)
+      new_img, normalized_img = delete_bg(specie_directory, index, type_img)
       file_path = f"{choose_final_path}/{specie}/{index}.jpg"
+      if generate_pickle.lower() == 'y':
+        data['label'].append(specie)
+        data['img_data'].append(normalized_img)
+
       with safe_open_w(file_path) as f:
         new_img.save(f)
-    print(f"[+] End with {specie}")
+    print(f"[+] End with {specie}\n\n")
+  
+  if generate_pickle.lower() == 'y':
+    print(f"data['img_data'].shape, {np.array(data['img_data']).shape}")
+    print(f"[+] Generate pickle")
+    prefix_data = 'all' if data_used == -1 else str(data_used)
+    path_pickle = f"{choose_final_path}/export/data_{prefix_data}_{type_img}.pkl"
+    os.makedirs(os.path.dirname(path_pickle), exist_ok=True)
+    joblib.dump(data, path_pickle)
+    print(f"[+] pickle save at {path_pickle}")
