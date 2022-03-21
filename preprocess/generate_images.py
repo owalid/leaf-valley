a1fe841f1b1a0f1f04a1c397ff3521c56a34fa6f
owalid <othmane.el.ayadi@gmail.com>
2022-03-21 18:33:03 +0100

    add select multiple for type of features

--- preprocess/generate_images.py
+++ preprocess/generate_images.py
@@ -10,6 +10,10 @@ import sys
 sys.path.append("/Users/agritech/vic-2_i/analysis/directory_data_repartitions")
 from utils import getDf
 import joblib
+from multiprocessing.pool import ThreadPool
+from skimage import feature
+from pprint import pprint
+import inquirer
 
 pcv.params.debug = ''
 debug = ''
@@ -21,16 +25,16 @@ ONLY_HEALTHY = 'ONLY_HEALTHY'
 NOT_HEALTHY = 'NOT_HEALTHY' 
 
 def getDataUsed(data_used, df, type_output):
-  data_used = data_used if data_used <= 1000 else -1
 
   if type_output == HEALTHY_NOT_HEALTHY:
     df_healthy = df.loc[df['healthy']]
     df_not_healthy = df.loc[df['healthy'] == False]
     return {
-        'healthy': data_used // len(df_healthy) if data_used != -1 else -1,
-        'not_healthy': data_used // len(df_not_healthy) if data_used != -1 else -1
-      }
+      'healthy': data_used // len(df_healthy) if data_used != -1 else -1,
+      'not_healthy': data_used // len(df_not_healthy) if data_used != -1 else -1
+    }
 
+  data_used = data_used if data_used <= 1000 else -1
   return data_used
 
 def get_df_filtered(df, type_output):
@@ -81,6 +85,44 @@ def safe_open_w(path):
     '''
     os.makedirs(os.path.dirname(path), exist_ok=True)
     return open(path, 'w')
+
+def build_filters_gabor():
+    filters = []
+    ksize = 31
+    for theta in np.arange(0, np.pi, np.pi / 16):
+        kern = cv2.getGaborKernel((ksize, ksize), 3.5, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
+        kern /= 1.5*kern.sum()
+        filters.append(kern)
+    return filters
+
+def process_gabor_threaded(img, filters, threadn = 8):
+    accum = np.zeros_like(img)
+    def f(kern):
+        return cv2.filter2D(img, cv2.CV_8UC3, kern)
+    pool = ThreadPool(processes=threadn)
+    for fimg in pool.imap_unordered(f, filters):
+        np.maximum(accum, fimg, accum)
+    return accum
+
+
+def gabor_process(img):
+  filters = build_filters_gabor()
+  return process_gabor_threaded(img, filters)
+
+def canny_process(img):
+  return pcv.canny_edge_detect(img, sigma=1.5)
+
+def get_textural_features(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    glcm = feature.graycomatrix(img, [1], [0], symmetric=True, normed=True)
+    dissimilarity = feature.graycoprops(glcm, 'dissimilarity')[0, 0]
+    correlation = feature.graycoprops(glcm, 'correlation')[0, 0]
+    homogeneity = feature.graycoprops(glcm, 'homogeneity')[0, 0]
+    energy = feature.graycoprops(glcm, 'energy')[0, 0]
+    ASM = feature.graycoprops(glcm, 'ASM')[0, 0]
+    contrast = feature.graycoprops(glcm, 'contrast')[0, 0]
+    ff = np.array([dissimilarity, correlation, homogeneity, energy, ASM, contrast])
+    return ff
 
 def get_mask_bg_disease(bgr_img, gray_img, specie):
   is_corn_disease = specie == 'corn'
@@ -207,25 +249,15 @@ def generate_img_without_bg(specie_directory, img_number, type_img, specie, heal
   if type_img == 'canny':
     edges = pcv.canny_edge_detect(np.array(im_s_1))
     normalized_img = cv2.normalize(edges, edges, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    return Image.fromarray(edges), cv2.resize(normalized_img, size_img)
-  elif type_img == 'color+canny':
-    array_img = np.array(im_s_1)
-    color_normalized_img = cv2.normalize(array_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-
-    edges = pcv.canny_edge_detect(np.array(im_s_1))
-    canny_normalized_img = cv2.normalize(edges, edges, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    canny_normalized_img = cv2.resize(canny_normalized_img, size_img)
-    color_normalized_img = cv2.resize(color_normalized_img, size_img)
-    # final_img = np.append(color_normalized_img, canny_normalized_img)
-    return color_normalized_img, canny_normalized_img
-  else:
+    return Image.fromarray(edges), cv2.resize(normalized_img, size_img), edges
+  else: # COLOR OR GRAY
     array_img = np.array(im_s_1)
     if type_img == 'color':
       normalized_img = cv2.normalize(array_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    else:
+    else: # GRAY
       normalized_img = cv2.normalize(array_img, array_img, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-    return im_s_1, cv2.resize(normalized_img, size_img)
+    return im_s_1, cv2.resize(normalized_img, size_img), cv2.resize(array_img, size_img)
 
 
 # MAIN
@@ -241,7 +273,7 @@ if __name__ == '__main__':
 
 
   type_output = input('Type of outputs \n0) healthy_not_healthy (default)\n1) only healthy\n2) only disases\n> ')
-  if type_output == '' or not type_output.isnumeric():
+  if type_output == '' or not type_output.isnumeric() or int(type_output) == 0:
     type_output = HEALTHY_NOT_HEALTHY
   elif int(type_output) == 1:
     type_output = ONLY_HEALTHY
@@ -251,17 +283,17 @@ if __name__ == '__main__':
   df_filtred = get_df_filtered(df, type_output)
   indexes_species = df_filtred.index
 
-  data_used = int(input('Choose number of images per class maximum 1000 (default maximum per class)\n> '))
-  data_used = getDataUsed(data_used, df_filtred, type_output)
+  data_used_raw = int(input('Choose number of images per class maximum 1000 (default maximum per class)\n> '))
+  data_used = getDataUsed(data_used_raw, df_filtred, type_output)
 
 
-  type_img_code = input(f'Choose your result type image: \n0) for canny_edge\n1) for gray_scale (default)\n2) for rgb\n3) for color + canny\n> ')
+
+  
+  type_img_code = input(f'Choose your result type image: \n0) for canny_edge\n1) for gray_scale (default)\n2) for rgb\n> ')
   if type_img_code == '0':
     type_img = 'canny'
   elif type_img_code == '2':
     type_img = 'color'
-  elif type_img_code == '3':
-    type_img = 'color+canny'
   else:
     type_img = 'gray'
   
@@ -272,8 +304,20 @@ if __name__ == '__main__':
   choose_final_path = default_path_result if choose_final_path == '' else choose_final_path
     
   generate_pickle = input(f'Want to generate pickle (default Y) ? Y/n\n> ')
-  generate_pickle = 'y' if generate_pickle == '' else generate_pickle
+  generate_pickle = 'y' if generate_pickle == '' else generate_pickle.lower()
 
+  if generate_pickle == 'y':
+    type_features = [
+        inquirer.Checkbox(
+            "type",
+            message="Choose your features in pickles:",
+            choices=["rgb", "gray", "canny", "gabor", "textural_feature"],
+        ),
+    ]
+
+    answers_type_features = inquirer.prompt(type_features)
+    answers_type_features = answers_type_features['type']
+    
 
   size_img = input(f'Size of output images (default 204px) ?\n> ')
   size_img = DEFAULT_FINAL_IMG_SIZE if size_img == '' or not size_img.isnumeric() else (int(size_img), int(size_img))
@@ -283,6 +327,9 @@ if __name__ == '__main__':
   data = dict()
   data['label'] = []
   data['img_data'] = []
+  data['gabor_img'] = []
+  data['canny_img'] = []
+  data['textural_feature'] = []
 
   print('\n')
   print("=====================================================")
@@ -291,6 +338,7 @@ if __name__ == '__main__':
   print(f"[+] size output images: {size_img}")
   print(f"[+] type image: {type_img}")
   print(f"[+] path: {choose_final_path}")
+  print(f"[+] answers_type_features: {answers_type_features}")
   print("=====================================================")
   for specie_directory in indexes_species:
     current_df = df_filtred.loc[specie_directory]
@@ -299,7 +347,7 @@ if __name__ == '__main__':
     specie = current_df.specie
 
     current_data_used = data_used if not isinstance(data_used, dict) else data_used['healthy' if healthy else 'not_healthy']
-    number_img = current_df.number_img if current_data_used == -1 else current_data_used
+    number_img = current_df.number_img if current_data_used == -1 or current_df.number_img < current_data_used else current_data_used
     print(f"[+] index {specie_directory}")
     print(f"[+] Start generate specie: {specie}")
     print(f"[+] Number of images: {number_img}")
@@ -312,11 +360,21 @@ if __name__ == '__main__':
     for index in range(1, number_img):
       if int(number_img / 2) == index:
         print("[+] 50%")
-      clean_pill_img, np_clean_img = generate_img_without_bg(specie_directory, index, type_img, specie, healthy, size_img)
+      clean_pill_img, np_clean_img, raw_np_img = generate_img_without_bg(specie_directory, index, type_img, specie, healthy, size_img)
       file_path = f"{choose_final_path}/{label}/{specie}-{disease}-{index}.jpg"
       if generate_pickle.lower() == 'y':
         data['label'].append(label)
-        data['img_data'].append(np_clean_img)
+
+        if 'rgb' in answers_type_features or len(answers_type_features) == 0:
+          data['img_data'].append(np_clean_img)
+        if 'gabor' in answers_type_features:
+          data['gabor_img'].append(gabor_process(np_clean_img))
+        if 'gray':
+          data['img_data'].append(np_clean_img)
+        if 'canny' in answers_type_features:
+          data['canny_img'].append(canny_process(np_clean_img))
+        if 'textural_feature' in answers_type_features:
+          data['textural_feature'].append(get_textural_features(raw_np_img))
 
       with safe_open_w(file_path) as f:
         clean_pill_img.save(f)
@@ -325,7 +383,7 @@ if __name__ == '__main__':
   if generate_pickle.lower() == 'y':
     print(f"data['img_data'].shape, {np.array(data['img_data']).shape}")
     print(f"[+] Generate pickle")
-    prefix_data = 'all' if data_used == -1 else str(data_used)
+    prefix_data = 'all' if int(data_used_raw) == -1 else str(data_used_raw)
     path_pickle = f"{choose_final_path}/export/data_{type_output.lower()}_{prefix_data}_{type_img}.pkl"
     os.makedirs(os.path.dirname(path_pickle), exist_ok=True)
     joblib.dump(data, path_pickle)
