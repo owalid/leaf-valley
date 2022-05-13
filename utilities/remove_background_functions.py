7ceb5aac48c45cf862c1491a48662edbf05e60d5
owalid <othmane.el.ayadi@gmail.com>
2022-05-13 11:24:38 +0200

    add random for selecting indexes

--- utilities/remove_background_functions.py
+++ utilities/remove_background_functions.py
@@ -8,11 +8,7 @@ from inspect import getsourcefile
 import os.path as path, sys
 current_dir = path.dirname(path.abspath(getsourcefile(lambda:0)))
 sys.path.insert(0, current_dir[:current_dir.rfind(path.sep)])
-<<<<<<< HEAD
-from utilities.utils import bgrtorgb, crop_resize_image
-=======
 from utilities.utils import bgrtorgb
->>>>>>> e4b6aa7 (fix error import)
 
 color_dict_HSV = {
     'black': [[180, 255, 26], [0, 0, 0]],
