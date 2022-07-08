f0519770ce24f4bdf588ec1d1e09c3a7d1963277
owalid <othmane.el.ayadi@gmail.com>
2022-07-08 12:21:14 +0200

    add new lab proc

--- process/deep_learning/models.py
+++ process/deep_learning/models.py
@@ -207,43 +207,247 @@ def lab_process(input_shape, num_classes):
     model = common_model_hsv_lab(lab_model, inputs, num_classes)
     return model
 
+
+def conv2d_bn(x, filters, num_row, num_col, padding='same', strides=(1, 1), name=None):
+    """
+        Utility function to apply conv + BN.
+    """
+    x = tfl.Conv2D(filters, (num_row, num_col), strides=strides, padding=padding, name=name)(x)
+    x = tfl.BatchNormalization()(x)
+    x = tfl.Activation('relu')(x)
+    return x
+
 def lab_inceptionv3_process(input_shape, num_classes):
     '''
         Inspiration: https://github.com/joaopauloschuler/two-path-noise-lab-plant-disease
     '''
     inputs = tf.keras.Input(shape=input_shape)
     
+    l_ratio = 0.2
+    ab_ratio = 0.8
+    
     # layer copies channels from channel_start the number of channels given in channel_count.
     l_input = CopyChannels(0,1)(inputs)
     ab_input = CopyChannels(1,2)(inputs)
     
     # L processing
-    l_proc = tfl.Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding="same")(l_input)
-    l_proc = tfl.Conv2D(filters=128, kernel_size=(5,5), strides=(1,1), padding="same")(l_input)
-    l_proc = tfl.Conv2D(filters=96, kernel_size=(3,3), strides=(1,1), padding="same")(l_input)
+    l_proc = tfl.Conv2D(filters=int(l_ratio*512), kernel_size=(21,21), strides=(2,2), padding="same")(l_input)
+    l_proc = tfl.BatchNormalization()(l_proc)
+    l_proc = tfl.Activation('relu')(l_proc)
+    
+    l_proc = tfl.Conv2D(filters=int(l_ratio*256), kernel_size=(11,11), strides=(1,1), padding="same")(l_input)
+    l_proc = tfl.BatchNormalization()(l_proc)
+    l_proc = tfl.Activation('relu')(l_proc)
+    
+    l_proc = tfl.Conv2D(filters=int(l_ratio*128), kernel_size=(5,5), strides=(1,1), padding="same")(l_input)
+    l_proc = tfl.BatchNormalization()(l_proc)
+    l_proc = tfl.Activation('relu')(l_proc)
+    
+    l_proc = tfl.Conv2D(filters=int(l_ratio*96), kernel_size=(3,3), strides=(1,1), padding="same")(l_input)
+    l_proc = tfl.BatchNormalization()(l_proc)
+    l_proc = tfl.Activation('relu')(l_proc)
+    
     l_proc = tfl.MaxPooling2D()(l_proc)
 
     # AB processing
-    ab_proc = tfl.Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding="same")(ab_input)
-    ab_proc = tfl.Conv2D(filters=128, kernel_size=(5,5), strides=(1,1), padding="same")(ab_input)
-    ab_proc = tfl.Conv2D(filters=96, kernel_size=(3,3), strides=(1,1), padding="same")(ab_input)
+    ab_proc = tfl.Conv2D(filters=int(512*ab_ratio), kernel_size=(21,21), strides=(2,2), padding="same")(ab_input)
+    ab_proc = tfl.BatchNormalization()(ab_proc)
+    ab_proc = tfl.Activation('relu')(ab_proc)
+    
+    ab_proc = tfl.Conv2D(filters=int(256*ab_ratio), kernel_size=(11,11), strides=(1,1), padding="same")(ab_input)
+    ab_proc = tfl.BatchNormalization()(ab_proc)
+    ab_proc = tfl.Activation('relu')(ab_proc)
+    
+    ab_proc = tfl.Conv2D(filters=int(128*ab_ratio), kernel_size=(5,5), strides=(1,1), padding="same")(ab_input)
+    ab_proc = tfl.BatchNormalization()(ab_proc)
+    ab_proc = tfl.Activation('relu')(ab_proc)
+    
+    ab_proc = tfl.Conv2D(filters=int(96*ab_ratio), kernel_size=(3,3), strides=(1,1), padding="same")(ab_input)
+    ab_proc = tfl.BatchNormalization()(ab_proc)
+    ab_proc = tfl.Activation('relu')(ab_proc)
+    
     ab_proc = tfl.MaxPooling2D()(ab_proc)
 
     # LAB concatenation
-    lab_model = tfl.concatenate([l_proc, ab_proc])
-    lab_model = tfl.Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same")(lab_model)
-    lab_model = tfl.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding="same")(lab_model)
-    # lab_model = tfl.MaxPooling2D()(lab_model)
+    x = tfl.concatenate([l_proc, ab_proc])
+    x = tfl.Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same")(x)
+    x = tfl.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding="same")(x)
     
-    inception_v3_model = tf.keras.applications.InceptionV3(input_shape=input_shape, include_top=False)
-    inception_v3_model.trainable = False
-    lab_model = inception_v3_model(lab_model)
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = tfl.AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 32, 1, 1)
+    x = tfl.concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool],
+                            axis=3,
+                            name='mixed0')
+
+    # mixed 1: 35 x 35 x 288
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = tfl.AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    x = tfl.concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool],
+                            axis=3,
+                            name='mixed1')
+
+    # mixed 2: 35 x 35 x 288
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = tfl.AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    x = tfl.concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool],
+                            axis=3,
+                            name='mixed2')
+
+    # mixed 3: 17 x 17 x 768
+    branch3x3 = conv2d_bn(x, 384, 3, 3, strides=(2, 2), padding='valid')
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(
+        branch3x3dbl, 96, 3, 3, strides=(2, 2), padding='valid')
+
+    branch_pool = tfl.MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = tfl.concatenate([branch3x3, branch3x3dbl, branch_pool],
+                            axis=3,
+                            name='mixed3')
     
-    lab_model = tfl.GlobalAveragePooling2D()(lab_model)
-    lab_model = tfl.Dense(512, activation='relu')(lab_model)
-    lab_model = tfl.Dropout(0.5)(lab_model)
+        
+    # mixed 4: 17 x 17 x 768
+    branch1x1 = conv2d_bn(x, 192, 1, 1)
+
+    branch7x7 = conv2d_bn(x, 128, 1, 1)
+    branch7x7 = conv2d_bn(branch7x7, 128, 1, 7)
+    branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+
+    branch7x7dbl = conv2d_bn(x, 128, 1, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 1, 7)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+
+    branch_pool = tfl.AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+    x = tfl.concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool],
+                            axis=3,
+                            name='mixed4')
+
+    # mixed 5, 6: 17 x 17 x 768
+    for i in range(2):
+        branch1x1 = conv2d_bn(x, 192, 1, 1)
+
+        branch7x7 = conv2d_bn(x, 160, 1, 1)
+        branch7x7 = conv2d_bn(branch7x7, 160, 1, 7)
+        branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+
+        branch7x7dbl = conv2d_bn(x, 160, 1, 1)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 7, 1)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 1, 7)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 7, 1)
+        branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+
+        branch_pool = tfl.AveragePooling2D((3, 3),
+                                            strides=(1, 1),
+                                            padding='same')(
+                                                x)
+        branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+        x = tfl.concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool],
+                            axis=3,
+                            name='mixed' + str(5 + i))
+
+    # mixed 7: 17 x 17 x 768
+    branch1x1 = conv2d_bn(x, 192, 1, 1)
+
+    branch7x7 = conv2d_bn(x, 192, 1, 1)
+    branch7x7 = conv2d_bn(branch7x7, 192, 1, 7)
+    branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+
+    branch7x7dbl = conv2d_bn(x, 192, 1, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
+    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+
+    branch_pool = tfl.AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+    x = tfl.concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool],
+                            axis=3,
+                            name='mixed7')
+
+    # mixed 8: 8 x 8 x 1280
+    branch3x3 = conv2d_bn(x, 192, 1, 1)
+    branch3x3 = conv2d_bn(branch3x3, 320, 3, 3, strides=(2, 2), padding='valid')
+
+    branch7x7x3 = conv2d_bn(x, 192, 1, 1)
+    branch7x7x3 = conv2d_bn(branch7x7x3, 192, 1, 7)
+    branch7x7x3 = conv2d_bn(branch7x7x3, 192, 7, 1)
+    branch7x7x3 = conv2d_bn(
+        branch7x7x3, 192, 3, 3, strides=(2, 2), padding='valid')
+
+    branch_pool = tfl.MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = tfl.concatenate([branch3x3, branch7x7x3, branch_pool],
+                            axis=3,
+                            name='mixed8')
+
+    # mixed 9: 8 x 8 x 2048
+    for i in range(2):
+        branch1x1 = conv2d_bn(x, 320, 1, 1)
+
+        branch3x3 = conv2d_bn(x, 384, 1, 1)
+        branch3x3_1 = conv2d_bn(branch3x3, 384, 1, 3)
+        branch3x3_2 = conv2d_bn(branch3x3, 384, 3, 1)
+        branch3x3 = tfl.concatenate([branch3x3_1, branch3x3_2],
+                                    axis=3,
+                                    name='mixed9_' + str(i))
+
+        branch3x3dbl = conv2d_bn(x, 448, 1, 1)
+        branch3x3dbl = conv2d_bn(branch3x3dbl, 384, 3, 3)
+        branch3x3dbl_1 = conv2d_bn(branch3x3dbl, 384, 1, 3)
+        branch3x3dbl_2 = conv2d_bn(branch3x3dbl, 384, 3, 1)
+        branch3x3dbl = tfl.concatenate([branch3x3dbl_1, branch3x3dbl_2],
+                                        axis=3)
+
+        branch_pool = tfl.AveragePooling2D((3, 3),
+                                            strides=(1, 1),
+                                            padding='same')(
+                                                x)
+        branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+        x = tfl.concatenate([branch1x1, branch3x3, branch3x3dbl, branch_pool],
+                            axis=3,
+                            name='mixed' + str(9 + i))
+        
+    
+    x = tfl.GlobalAveragePooling2D()(x)
+    x = tfl.Dense(512, activation='relu')(x)
+    x = tfl.Dropout(0.5)(x)
     prediction_layer = tfl.Dense(num_classes, activation='softmax')
-    outputs = prediction_layer(lab_model)
+    outputs = prediction_layer(x)
     model = tf.keras.Model(inputs, outputs)
     print(model.summary())
     
@@ -263,10 +467,10 @@ base_models = {
         'base': tf.keras.applications.ResNet50,
         'preprocess_input': tf.keras.applications.resnet50.preprocess_input
     },
-    'CONVNEXT': {
-        'base': tf.keras.applications.convnext,
-        'preprocess_input': tf.keras.applications.convnext.preprocess_input
-    },
+    # 'CONVNEXT': {
+    #     'base': tf.keras.applications.convnext,
+    #     'preprocess_input': tf.keras.applications.convnext.preprocess_input
+    # },
     'RESNET50V2': {
         'base': tf.keras.applications.ResNet50V2,
         'preprocess_input': tf.keras.applications.resnet_v2.preprocess_input
