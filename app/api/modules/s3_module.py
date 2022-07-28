ab56157c5f85962356d4a1c62a2357bc7cb1bdab
owalid <othmane.el.ayadi@gmail.com>
2022-07-28 18:07:27 +0200

    add session

--- app/api/modules/s3_module.py
+++ app/api/modules/s3_module.py
@@ -38,11 +38,11 @@ class S3Module():
         self.files_leafs = self.get_files_leafs()
     
     def get_s3_client(self):
-        return boto3.client('s3', endpoint_url=self.S3_BASE_ENDPOINT_URL)
+        return self.session.client('s3', endpoint_url=self.S3_BASE_ENDPOINT_URL)
     
     def get_s3_resource(self):
-        session = boto3.Session(aws_access_key_id=self.S3_ACCESS_KEY_ID, aws_secret_access_key=self.S3_SECRET_ACCESS_KEY)
-        return session.resource('s3', endpoint_url=self.S3_BASE_ENDPOINT_URL)
+        # session = boto3.Session(aws_access_key_id=self.S3_ACCESS_KEY_ID, aws_secret_access_key=self.S3_SECRET_ACCESS_KEY)
+        return self.session.resource('s3', endpoint_url=self.S3_BASE_ENDPOINT_URL)
     
     def get_models_list(self):
         model_bucket = self.s3_resource.Bucket(self.S3_BUCKET_NAME)
