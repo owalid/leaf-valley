ce520eecd2f5bef94a9dc64714e8304fe5c7311b
agritech <kchadir@student.42.fr>
2022-08-16 09:33:44 +0200

    wip 220816

--- process/ml_classification/main.py
+++ process/ml_classification/main.py
@@ -4,13 +4,14 @@
 import os
 import sys
 import random
-import argparse as ap
-from inspect import getsourcefile
+import joblib
 
 import pandas as pd
 import numpy as np
 
-from sklearn.utils import shuffle
+import argparse as ap
+from inspect import getsourcefile
+
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder, OneHotEncoder
 
 import xgboost as xgb
@@ -27,24 +28,111 @@ import load_data_from_h5
 
 scaler_dict = dict(('NORM_MINMAX', MinMaxScaler()), ('NORM_STANDARSCALER', StandardScaler()))
 
-def set_models(classType, classModels, verbose):
+def set_models_dict(classType, classModels, verbose):
   xgc = xgb.XGBClassifier(use_label_encoder=False, objective='binary:logistic' if classType == 'HEALTHY' else 'multi:softmax', eval_metric=f1_score, n_estimators=500, n_jobs=-1, verbose=verbose)
   rfc = RandomForestClassifier(n_estimators=500, n_jobs=-1, verbose=verbose)
   etc = ExtraTreesClassifier(n_estimators=500, n_jobs=-1, verbose=verbose)
   _dict = dict(('xgc', xgc()), ('rfc', rfc()), ('etc', etc()))
   return { md: _dict[md.lower()] for md in classModels}
 
+def split_data(df):
+    features = [f for f in df.columns if f not in ['classes','split']]
+    X_train = df.loc[df.split=='train'][features]
+    y_train = df.loc[df.split=='train']['classes']
+    X_test  = df.loc[df.split=='test'][features]
+    y_test  = df.loc[df.split=='test']['classes']
+    return X_train, y_train, X_test, y_test
+
+# Datat normalization
+def data_normalization(df, scaler, save=False, fit=False):
+  # fit data normalization
+  if fit:
+    scaler.fit(df)
+
+  # transform data normalization
+  return scaler, scaler.transform(df).astype(np.float32)
+
+# Label encoding
+def label_encoding(df, le, class_type, save=False, fit=False):
+  # Class agregation
+  if class_type == 'HEALTHY':
+    df_features['classes'] = df_features.classes.apply(lambda c: 'healthy' if 'healthy' in c.lower() else 'not_healthy')
+  if class_type == 'PLANTS':
+    df_features['classes'] = df_features.classes.apply(lambda c: c.split('___')[0])
+
+  # fit data normalization
+  if fit:
+    le.fit(df)
+
+  # transform data normalization
+  return le, le.transform(df)
+
+# Save model
+def save_model_func(model, scaler, le, features,class_type, md_label, dst_path):
+    path = os.path.join(dst_path, f'ML_{md_label.upper()}_{class_type}.pkl.z')
+    if os.path.exists(path):
+        os.remove(path)
+
+    md_dict = {}
+    md_dict['ml_model'] = model
+    md_dict['ml_scaler'] = scaler
+    md_dict['ml_label_encoder'] = le
+    md_dict['ml_features'] = features
+    joblib.dump(md_dict, path)
+    local_print(f'Info : model {md_label} has been saved !!!§')
+
+# fit models
+def fit_models(X_train, y_orig, classification_models, classification_types, save, options_dataset, verbose):
+  # Data normalization
+  scaler, X_train = data_normalization(X_train, scaler_dict[normalize_type], save=save, fit=True)
+
+  # Processing
+  models_dict = {}
+  for class_type in classification_types:
+    # Label encoding
+    le, y_train = label_encoding(y_orig, LabelEncoder(), fit=True)
+
+    # Set models dictionary
+    models_dict[class_type] = set_models_dict(class_type, classification_models, verbose)
+
+    # Fit models
+    for md in models_dict[class_type].keys():
+        models_dict[class_type][md].fit(X_train, y_train)
+        if save:
+          save_model_func(models_dict[class_type][md], scaler, le, X_train.columns, class_type, md, options_dataset, 'data/models', verbose)
+
+# predict models
+def predict_models(X_test, y_orig, classification_models, verbose):
+  # Data normalization
+  scaler, X_train = data_normalization(X_test, scaler_dict[normalize_type])
+
+  # Processing
+  models_dict = {}
+  for class_type in classification_types:
+    # Label encoding
+    le, y_train = label_encoding(y_orig, LabelEncoder(), fit=True)
+
+    # Set models dictionary
+    models_dict[class_type] = set_models_dict(class_type, classification_models, verbose)
+
+    # Fit models
+    for md in models_dict[class_type].keys():
+        models_dict[class_type][md].fit(X_train, y_train)
+        if save:
+          save_model_func(models_dict[class_type][md], scaler, le, X_train.columns, class_type, md, options_dataset, 'data/models', verbose)
+
+
 # MAIN
 if __name__ == '__main__':
     parser = ap.ArgumentParser(formatter_class=ap.RawTextHelpFormatter)
     parser.add_argument("-src", "--src-directory", required=False, type=str, default='data/preprocess',
-                        help='Directory source who can find data for the classification. default: data/process/ml_classification')
+                        help='Directory source who can find data for the classification. default: data/preprocess/ml_classification')
     parser.add_argument("-f", "--filename", required=True, type=str, 
-                        help='Filename input data format h5 for raw data or pkl for train/test data')
-    parser.add_argument("-dst", "--destination", required=False, type=str, default='data/process/ml_classification',
-                        help='Path to save the data. default: data/process/ml_classification')
+                        help='Basename of the file input data without extension')
+    parser.add_argument("-dst", "--preprocessed-data", required=False, type=str, default='data/preprocess/ml_classification',
+                        help='Path to save or to get the preprocessed data. default: data/preprocess/ml_classification')
     parser.add_argument("-sd", "--save-data", required=False,
-                        action='store_true', default=False, help='Save train/test data and options_datasets')
+                        action='store_true', default=False, help='Save options_datasets json file and converted data from h5 format to DataFrame one with flag train/test flag')
     parser.add_argument("-nortype", "--normalize-type", required=False, type=str, default='NORM_MINMAX',
                         help='Normalize data (NORM_STANDARSCALER or NORM_MINMAX normalization) (Default: NORM_MINMAX)')
     parser.add_argument("-cs", "--classification-step", required=False, type=str, default="ALL",
@@ -66,10 +154,10 @@ if __name__ == '__main__':
     print(args)
 
     src_directory         = args.src_directory
-    filename              = args.filename
-    destination           = args.destination
+    file_basename         = args.filename
+    preprocessed_data           = args.preprocessed_data
     classification_step   = args.classification_step
-    classification_type   = args.classification_type
+    classification_types  = ['PLANTS', 'HEALTHY', 'PLANTS_DESEASES'] if args.classification_type.upper() == 'ALL' else args.classification_type.split(',')
     classification_models = ['XGB', 'ETC', 'RFC'] if args.classification_models.upper() == 'ALL' else args.classification_models.split(',')
     normalize_type        = args.normalize_type
     save_data             = args.save_data
@@ -78,62 +166,42 @@ if __name__ == '__main__':
     save_scores           = args.save_scores
     verbose               = args.verbose
 
-    file_basename = filename.split('.')[0]
-
-    if not os.path.exists(destination):
-      os.makedirs(destination)
-      local_print(f'\nInfo : folder {destination} was created successfully !!\n', verbose)
+    if not os.path.exists(preprocessed_data):
+      os.makedirs(preprocessed_data)
+      local_print(f'\nInfo : folder {preprocessed_data} was created successfully !!\n', verbose)
 
     if classification_step in ['ALL', 'LOAD_DATA'] :
-      if not (filename.split('.')[-1] == 'h5' and os.path.exists(os.path.join(src_directory, filename))): 
-        print(f'Error : filename ({filename}) not a h5 file or doesn\'t exist')
+      if not os.path.exists(os.path.join(src_directory, f'{file_basename}.h5')):
+        print(f'Error : filename ({file_basename}.h5) not a h5 file or doesn\'t exist')
         exit(1)
       else:
-        df_features, options_dataset = load_data_from_h5(src_directory, filename, verbose)
-        # SPlit manually data into train/test to save it
-        df_features = shuffle(df_features)
-
-        df_features.loc[random.sample(df_features.index.to_list(), int(.7*len(df_features))),'split'] = 'train'
-        df_features['split'].fillna('test', inplace=True)
+        df_features, options_dataset = load_data_from_h5(src_directory, f'{file_basename}.h5', verbose)
         
         if save_data:
-          df_features.to_pickle(os.path.join(destination, file_basename+".pkl"))
+          df_features.to_pickle(os.path.join(preprocessed_data, file_basename+".pkl"))
+          joblib.dump(options_dataset, os.path.join(preprocessed_data, file_basename+"_options_dataset.joblib"))
         
         if classification_step == 'LOAD_DATA':
           local_print('Info : end of the load data job')
           exit(0)
 
-    if not os.path.exists(os.path.join(destination, file_basename+".pkl")):
-        local_print(f'Error : the file {file_basename}.pkl does not exist')
-        exit(1)
-    
-    else:
-      # load data if not yet loaded
-      if classification_step in ['FIT_MODEL', 'PREDICT_MODEL', 'FIT_PREDICT_MODEL'] :
-        df_features = pd.read_pickle(os.path.join(destination, file_basename+".pkl"))
+    # load data if not yet loaded
+    if classification_step in ['FIT_MODEL', 'PREDICT_MODEL', 'FIT_PREDICT_MODEL'] :
+        if not os.path.exists(os.path.join(preprocessed_data, file_basename+".pkl")):
+            local_print(f'Error : the file {file_basename}.pkl does not exist')
+            exit(1)
+        df_features = pd.read_pickle(os.path.join(preprocessed_data, file_basename+".pkl"))
+        options_dataset =  joblib.load(os.path.join(preprocessed_data, file_basename+"_options_dataset.joblib"))
 
-      # data normalization
-      features = [f for f in df_features.columns if f not in ['classes','split']]
-      scaler = scaler_dict[normalize_type] 
-      scaler.fit(df_features[features])
-      df_features[features]=scaler.transform(df_features[features]).astype(np.float32)
+    # Split data
+    X_train, y_train, X_test, y_test = split_data(df_features)
 
-      # Class agregation
-      if classification_type == 'HEALTHY':
-        df_features['classes'] = df_features.classes.apply(lambda c: 'healthy' if 'healthy' in c.lower() else 'not_healthy')
-      if classification_type == 'PLANTS':
-        df_features['classes'] = df_features.classes.apply(lambda c: c.split('___')[0])
-      
-      # Label Encoding
-      le = LabelEncoder()
-
-      # Coding of the label for plants with possible diseases
-      le.fit(df_features.classes)
-      df_features['label'] = le.transform(df_features.classes)
-
-      # Set models
-      models_dict = set_models(classification_type, classification_models, verbose)
-
+    if classification_step != 'PREDICT_MODEL':
+      # fit models
+      fit_models(X_train, y_train, classification_models, classification_types, save_model, options_dataset, verbose)
+      if classification_step == 'FIT_MODEL':
+          local_print('Info : end of the models fit', verbose)
+          exit(0)
 
 
 
@@ -143,4 +211,3 @@ if __name__ == '__main__':
 
 
     random.seed(42)
-
