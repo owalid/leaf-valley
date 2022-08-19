f268e53e26efaf186424285975d2ddd293cf93e4
owalid <othmane.el.ayadi@gmail.com>
2022-08-19 13:57:34 +0200

    fix predict

--- app/api/controllers/prediction.py
+++ app/api/controllers/prediction.py
@@ -357,6 +357,7 @@ class PredictionController:
         y = model.predict(images)
         del images
 
+        print(y.shape)
         df['prediction_label'] = [class_names[le] for le in np.argmax(y, axis=1)]
         df['proba'] = y.max(axis=1)
 
