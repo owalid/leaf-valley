8cb400438e574d40511f5010c4bca1213ed09dd3
owalid <othmane.el.ayadi@gmail.com>
2022-08-23 17:54:26 +0200

    fix self signed ssr requests

--- app/client/nuxt.config.js
+++ app/client/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/vuetify'],
+  plugins: ['~/plugins/vuetify', '~/plugins/axios'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
