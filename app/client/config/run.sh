da44b8dac6800cc29875124cf927da4bed4e86c2
owalid <othmane.el.ayadi@gmail.com>
2022-09-05 21:32:11 +0200

    update script run.sh

--- app/client/config/run.sh
+++ app/client/config/run.sh
@@ -1,5 +1,16 @@
 # get last line logs file
-LAST_ACCESS_LOG=$(echo -n $(cat server-middleware/log/access.log | tail -1))
+LAST_ACCESS_LOG=$(date -d $(echo -n $(cat server-middleware/log/access.log | tail -1)) +%s)
+NOW=$(date +"%s") - 3600
 
 # compare to now if > 1 hour stop instance
+if [[ "$NOW" > "$LAST_ACCESS_LOG" ]] ;
+then
+  RESULT_GET=$(curl -H "X-Auth-Token:$authToken" "https://api.scaleway.com/instance/v1/zones/$zone/servers/action?name=instance-cluster-api-leaf")
+  SERVER_ID=$(echo $RESULT_GET | jq'[].server_id')
+  STATE=$(echo $RESULT_GET | jq'[].state')
 
+  if [[ "$STATE" != "running" ]] ;
+  then
+    curl -X POST -d "action=poweroff" -H "X-Auth-Token:$authToken" "https://api.scaleway.com/instance/v1/zones/$zone/servers/$SERVER_ID/action"
+  fi
+fi
