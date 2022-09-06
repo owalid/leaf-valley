cd2b3c7b89c17d119a67cfdfe204f22e23c5e32e
owalid <othmane.el.ayadi@gmail.com>
2022-09-06 19:00:58 +0200

    clean code

--- app/client/config/run.sh
+++ app/client/config/run.sh
@@ -5,12 +5,12 @@ NOW=$(date -d "@$(($(date +%s) - 3600))") # now - 1 hour
 # compare to now - 1 hour with the last entry in log file.
 if [[ "$NOW" > "$LAST_ACCESS_LOG" ]] ;
 then
-  SERVERS_LIST=$(curl -H "X-Auth-Token:$SCW_AUTH_TOKEN" "https://api.scaleway.com/instance/v1/zones/$ZONE/servers?name=$SERVER_NAME")
+  SERVERS_LIST=$(curl -H "X-Auth-Token:$SCW_AUTH_TOKEN" "https://api.scaleway.com/instance/v1/zones/$SCW_SERVER_ZONE/servers?name=$SCW_NAME_INSTANCE")
   SERVER_ID=$(echo -n $(echo $SERVERS_LIST | jq -r '.[] | .[] | .id'))
   STATE=$(echo -n $(echo $SERVERS_LIST | jq -r '.[] | .[] | .state'))
 
   if [[ "$STATE" == "running" ]] ;
   then
-    curl -X POST -d "action=poweroff" -H "X-Auth-Token:$AUTH_TOKEN" "https://api.scaleway.com/instance/v1/zones/$ZONE/servers/$SERVER_ID/action"
+    curl -X POST -d "action=poweroff" -H "X-Auth-Token:$AUTH_TOKEN" "https://api.scaleway.com/instance/v1/zones/$SCW_SERVER_ZONE/servers/$SERVER_ID/action"
   fi
 fi
