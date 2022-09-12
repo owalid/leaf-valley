7efcf549d36a2fe5d7cdcd7ec9af0414501b7be2
owalid <othmane.el.ayadi@gmail.com>
2022-09-12 23:49:46 +0200

    fix condition

--- app/client/config/run.sh
+++ app/client/config/run.sh
@@ -4,7 +4,7 @@ LAST_ACCESS_LOG=$(date -d "$(echo -n $(cat /home/app/server-middleware/log/acces
 NOW=$(date -d "@$(($(date +%s) - 3600))" +%s) # now - 1 hour
 
 # compare to now - 1 hour with the last entry in log file.
-if [[ "$NOW" > "$LAST_ACCESS_LOG" ]] ;
+if [[ "$LAST_ACCESS_LOG" > "$NOW" ]] ;
 then
   SERVERS_LIST=$(curl -H "X-Auth-Token:$SCW_AUTH_TOKEN" "https://api.scaleway.com/instance/v1/zones/$SCW_SERVER_ZONE/servers?name=$SCW_NAME_INSTANCE")
   SERVER_ID=$(echo -n $(echo $SERVERS_LIST | jq -r '.[] | .[] | .id'))
