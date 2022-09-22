8f5dc510fd0c17135f3d40db51627371136996c4
owalid <othmane.el.ayadi@gmail.com>
2022-09-22 15:38:46 +0200

    add favicon

--- app/client/nuxt.config.js
+++ app/client/nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        titleTemplate: '%s - vic2i',
-        title: 'vic2i',
+        titleTemplate: '%s - Leaf valley',
+        title: 'Leaf valley',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
