793d19b000223c7b08deb963373776bc5cdb140b
owalid <othmane.el.ayadi@gmail.com>
2022-09-22 13:31:24 +0200

    add deploiment

--- app/client/nuxt.config.js
+++ app/client/nuxt.config.js
@@ -55,12 +55,14 @@ export default {
             pathRewrite: {
                 '^/api': '/api',
             },
+            changeOrigin: true,
         },
         '/econome': {
             target: process.env.NUXT_ECONOME_MS_URL || 'http://127.0.0.1:8080/econome',
             pathRewrite: {
                 '^/econome': '/econome',
             },
+            changeOrigin: true,
         },
     },
 
